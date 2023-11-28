5bf48d97ae248d56b084d18bfbf84252cf0f76e2
Diemisson Lima <131544547+diemissonlima@users.noreply.github.com>
2023-11-28 16:36:00 -0400

    script de movimentacao

---
+++ scripts/player/soldier.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+class_name Soldier
+
+onready var move_state: Node = get_node("States/Move")
+onready var attack_state: Node = get_node("States/Attack")
+
+onready var texture: Sprite = get_node("Texture")
+
+var is_crawling: bool = false
+var is_attacking: bool = false
+
+func _physics_process(_delta: float) -> void:
+	move_state.move()
+	texture.animate(move_state.velocity)
+	#attack_state.attack()
+	
---
+++ scripts/player/soldier_texture.gd
@@ -0,0 +1,42 @@
+extends Sprite
+class_name SoldierTexture
+
+var last_direction_state: bool
+
+export(NodePath) onready var parent = get_node(parent) as KinematicBody2D
+export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
+
+func animate(velocity: Vector2) -> void:
+	flip_h = set_orientation()
+	move_behavior(velocity)
+	
+
+func set_orientation() -> bool:
+	var mouse_global_position: Vector2 = get_global_mouse_position()
+	if mouse_global_position.x > parent.global_position.x:
+		last_direction_state = false
+		
+		return false
+		
+	if mouse_global_position.x < parent.global_position.x:
+		last_direction_state = true
+		
+		return true
+	
+	return last_direction_state
+	
+
+func move_behavior(velocity: Vector2) -> void:
+	if velocity != Vector2.ZERO:
+		animation.play(get_move_state())
+		return
+		
+	animation.play("idle")
+	
+
+func get_move_state() -> String:
+	if parent.is_crawling:
+		return "crawl"
+		
+	return "walk"
+	
