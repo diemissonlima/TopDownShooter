77711ad9cad9daad890cf665a310810ebd64a83d
Diemisson Lima <131544547+diemissonlima@users.noreply.github.com>
2023-12-12 18:00:34 -0400

    spawn dos inimigos + camadas e mascaras

--- scripts/management/game_level.gd
+++ scripts/management/game_level.gd
@@ -3,12 +3,39 @@ class_name GameLevel
 
 const CELL_SIZE: int = 16
 
+var avaliable_spawn_tiles_list: Array = []
+
 onready var assault: KinematicBody2D = get_node("LayeredObjects/Assault")
+onready var layered_objects: YSort = get_node("LayeredObjects")
 onready var navigation: Navigation2D = get_node("Terrain")
 onready var base_terrain: TileMap = get_node("Terrain/BaseTerrain")
 
 func _ready() -> void:
 	define_camera_limit()
+	set_spawn_tiles()
+	
+	
+func set_spawn_tiles() -> void:
+	for tile in base_terrain.get_used_cells():
+		find_edges(tile)
+		
+	layered_objects.avaliable_cells = avaliable_spawn_tiles_list
+		
+		
+func find_edges(current_cell: Vector2) -> void:
+	var used_rect: Rect2 = base_terrain.get_used_rect()
+	
+	var x_size: float = used_rect.size.x
+	var y_size: float = used_rect.size.y
+	
+	var x_pos: float = used_rect.position.x
+	var y_pos: float = used_rect.position.y
+	
+	if current_cell.x == x_size -1 or current_cell.x == x_pos:
+		avaliable_spawn_tiles_list.append(current_cell)
+	
+	if current_cell.y == y_size -1 or current_cell.y == y_pos:
+		avaliable_spawn_tiles_list.append(current_cell)
 	
 	
 func define_camera_limit() -> void:
---
+++ scripts/management/layered_objects.gd
@@ -0,0 +1,31 @@
+extends YSort
+class_name LayeredObjects
+
+const CELL_SIZE: int = 16
+
+onready var parent: Node2D = get_parent()
+onready var timer: Timer = get_node("Timer")
+
+var avaliable_cells: Array = []
+var spawn_cooldown: float = 1.5
+
+export(Array, PackedScene) var enemies_list
+
+func _ready() -> void:
+	randomize()
+
+
+func on_spawn_timer_timeout() -> void:
+	spawn()
+	timer.start(spawn_cooldown)
+	
+	
+func spawn() -> void:
+	var random_spawn_index: int = randi() % avaliable_cells.size()
+	var spawn_position: Vector2 = avaliable_cells[random_spawn_index] * CELL_SIZE
+	
+	var random_enemy_index: int = randi() % enemies_list.size()
+	var enemy = enemies_list[random_enemy_index].instance()
+	enemy.global_position = spawn_position
+	add_child(enemy)
+	
