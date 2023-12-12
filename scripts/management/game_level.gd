974d81f0e9f69cd35ff1f5a3234fdaa7df8fb2a4
Diemisson Lima <131544547+diemissonlima@users.noreply.github.com>
2023-12-12 11:16:24 -0400

    código do inimigo

--- scripts/management/game_level.gd
+++ scripts/management/game_level.gd
@@ -4,6 +4,7 @@ class_name GameLevel
 const CELL_SIZE: int = 16
 
 onready var assault: KinematicBody2D = get_node("LayeredObjects/Assault")
+onready var navigation: Navigation2D = get_node("Terrain")
 onready var base_terrain: TileMap = get_node("Terrain/BaseTerrain")
 
 func _ready() -> void:
@@ -19,4 +20,14 @@ func define_camera_limit() -> void:
 	
 	soldier_camera.limit_right = int(used_rect_tile.size.x * CELL_SIZE)
 	soldier_camera.limit_bottom = int(used_rect_tile.size.y * CELL_SIZE)
+	
+func _process(_delta: float) -> void:
+	if assault == null:
+		return
+		
+	send_player()
+	
+
+func send_player() -> void:
+	get_tree().call_group("enemy", "get_player", assault, navigation)
 	
